4929215feed5d84dc994d1221aff76e4581c206f
ruidanwang <newbiner@gmail.com>
2025-06-09 14:58:44 +0800

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,9 +1,13 @@
 Draw.loadPlugin(function(ui) {
   ui.actions.addAction('sayHello', function() {
-    alert('你好，Draw.io 插件测试成功！');
+    alert('你好，这是插件测试！');
   });
 
-  ui.menus.get('extras').funct = function(menu, parent) {
+  // 添加到“帮助”菜单中
+  var menu = ui.menus.get('help');
+  var oldFunct = menu.funct;
+  menu.funct = function(menu, parent) {
+    oldFunct.apply(this, arguments);
     ui.menus.addMenuItem(menu, 'sayHello');
   };
 });
